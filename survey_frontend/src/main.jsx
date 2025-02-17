0aa95fd78578d1e706bdf679291d73e7f6a005e7
Jack-Wood-Outernet <jack.wood@outernetglobal.com>
2025-02-17 17:35:53 +0000

    adding seperate pages

--- survey_frontend/src/main.jsx
+++ survey_frontend/src/main.jsx
@@ -1,13 +1,31 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
 import "./index.css";
 import Select_metaphor from "./pages/Select_metaphor.jsx";
 import { Provider } from "@/components/ui/provider";
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./pages/app.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/select",
+    element: <Select_metaphor />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
     <Provider>
-      <Select_metaphor />
+      {" "}
+      <RouterProvider router={router}></RouterProvider>
     </Provider>
-  </StrictMode>
+  </React.StrictMode>
 );
